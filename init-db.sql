3a94baf7f29715f865d2f4359b8c711c8112a95c
Muhammad Umair Khan <umair.anwar@xflowresearch.com>
2026-06-27 23:36:33 +0500

    configure Grafana to use Postgres as internal database

--- init-db.sql
+++ init-db.sql
@@ -7,3 +7,8 @@ GRANT ALL PRIVILEGES ON DATABASE kong TO kong;
 CREATE USER keycloak WITH PASSWORD 'keycloakpass';
 CREATE DATABASE keycloak OWNER keycloak;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+
+-- Create database and user for Grafana
+CREATE USER grafana WITH PASSWORD 'grafanapass';
+CREATE DATABASE grafana OWNER grafana;
+GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana;
